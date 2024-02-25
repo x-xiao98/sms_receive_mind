d181dc4f69e2ea8e34ba7395e92b4821f4bcdb23
xuanxx <xuanxx@bjchuangzhi.com>
2024-02-25 15:24:37 +0800

    测试代码

--- app/src/androidTest/java/com/example/demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/demo/ExampleInstrumentedTest.java
@@ -22,10 +22,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.demo", appContext.getPackageName());
 	assertEquals("com.example.demo", "com.example.demo");
-	assertEquals("com.example.demo", "com.example.demo");
-	assertEquals("com.example.demo", "com.example.demo");
-
-
-
     }
 }
